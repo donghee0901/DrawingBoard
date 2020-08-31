3f4558e5d413a1f186739c320ea74dadd3ecb2ff
KimDongHee <donghee0901@naver.com>
2020-08-31 18:59:59 +0900

    Success drawing
    draw line and rectangle

--- DrawingBoard/MainWindow.xaml.cs
+++ DrawingBoard/MainWindow.xaml.cs
@@ -23,6 +23,29 @@ namespace DrawingBoard
         public MainWindow()
         {
             InitializeComponent();
+            MainCanvas.Children.Add(DrawLine(0, 0, 100, 100));
+            MainCanvas.Children.Add(DrawRectangle(200, 100, 300, 300));
+        }
+        public Rectangle DrawRectangle(int x1, int y1, int x2, int y2)
+        {
+            Rectangle drawRectangle = new Rectangle();
+            drawRectangle.Width = x2 - x1;
+            drawRectangle.Height = y2 - y1;
+            drawRectangle.Fill = Brushes.Black;
+            Canvas.SetLeft(drawRectangle, x1);
+            Canvas.SetTop(drawRectangle, y1);
+            return drawRectangle;
+        }
+        public Line DrawLine(int x1,int y1,int x2,int y2)
+        {
+            Line drawLine = new Line();
+            drawLine.X1 = x1;
+            drawLine.Y1 = y1;
+            drawLine.X2 = x2;
+            drawLine.Y2 = y2;
+            drawLine.Stroke = Brushes.Red;
+            drawLine.StrokeThickness = 2;
+            return drawLine;
         }
     }
 }
